ce0d0a45deee835b45cadb20e8de2333ca96ac2f
Bryan W. Weber <bryan.w.weber@gmail.com>
2016-10-25 12:30:23 -0400

    [cantera-dev] Add boost dependency

--- cantera-dev/bld.bat
+++ cantera-dev/bld.bat
@@ -41,6 +41,7 @@ ECHO matlab_toolbox='n' >> cantera.conf
 ECHO debug='n' >> cantera.conf
 ECHO f90_interface='n' >> cantera.conf
 ECHO system_sundials='n' >> cantera.conf
+ECHO boost_inc_dir='%PREFIX\include' >> cantera.conf
 
 :: Select which version of the interface should be built
 IF "%PY_MAJ_VER%" EQU "2" GOTO PYTHON2
